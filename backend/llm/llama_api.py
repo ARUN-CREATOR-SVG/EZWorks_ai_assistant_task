0e6862e2f13669bf1412076033f7d022bda3ba51
Arun Singh <arunsin2212@gmail.com>
2025-06-24 18:12:21 +0530

    added llama llm HuggingFaceEndPoint

---
+++ backend/llm/llama_api.py
@@ -0,0 +1,30 @@
+from langchain.llms import HuggingFaceEndpoint
+from langchain.prompts import PromptTemplate
+from langchain_core.output_parsers import StrOutputParser
+
+
+llm = HuggingFaceEndpoint(
+    repo_id='meta-llama/Meta-Llama-3-8B-Instruct',
+    task='text-generation',
+    max_new_tokens=100
+)
+
+
+prompt = PromptTemplate(
+    template="""
+      You are a helpful assistant.
+      Answer ONLY from the provided transcript context.
+      If the context is insufficient, just say you don't know.
+
+      {context}
+      Question: {question}
+    """,
+    input_variables=["context", "question"]
+)
+
+parser = StrOutputParser()
+
+chain = prompt | llm | parser
+
+def call_llama_with_context(context, question):
+    return chain.run({"context": context, "question": question}).strip()
