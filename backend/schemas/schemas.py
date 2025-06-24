f11bfbaa33d5334bf034f5d3ffb54f3869c91dfa
Arun Singh <arunsin2212@gmail.com>
2025-06-24 22:02:19 +0530

    Added schemas for challengerequest and evaluaterequest

--- backend/schemas/schemas.py
+++ backend/schemas/schemas.py
@@ -4,3 +4,12 @@ from typing import Optional
 class AskRequest(BaseModel):
     question: str
     filename: Optional[str] = None
+
+
+class EvaluateRequest(BaseModel):
+    q: str
+    user_answer: str
+    filename: Optional[str] = None
+
+class ChallengeRequest(BaseModel):
+    filename: str
